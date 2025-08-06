3bc01d685b3ef21be797bed7567562db1ba3b2b2
Guibs DEV <90661841+GuibsDEV@users.noreply.github.com>
2025-08-05 22:33:32 -0300

    Fix integration syntax error and dependency conflict

--- src/components/marketing/integration.tsx
+++ src/components/marketing/integration.tsx
@@ -93,7 +93,6 @@ const Integration = () => {
                             </div>
                         </div>
                     </div>
-                </h2>
             </Container>
         </div>
     )
